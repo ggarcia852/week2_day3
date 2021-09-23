3ce4062f7842816e3b432f18afa4a732bef661ba
George Garcia <70930059+ggarcia852@users.noreply.github.com>
2021-09-22 21:34:44 -0600

    homework completed

---
+++ homework.py
@@ -0,0 +1,14 @@
+
+
+def area_of_house(length, width):
+    return length * width
+
+
+area_of_house(5, 2)
+
+
+from math import pi
+
+
+def circumference_of_circle(radius):
+     return 2 * pi * radius
